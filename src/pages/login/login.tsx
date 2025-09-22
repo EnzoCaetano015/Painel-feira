72afa99267f66ef134ac45465d6c318ab26d76a5
enzocaetano015 <peracioenzo@gmail.com>
2025-09-22 12:39:34 -0300

    tela de login

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,5 +1,23 @@
+import { Button, Stack, TextField } from "@mui/material"
+
 export const Login = () => {
     return (
-        <></>
+        <Stack sx={{
+            borderRadius: 2,
+            minWidth: 300,
+            maxWidth: 400,
+            boxShadow: 3,
+            p: 3,
+            gap: 3,
+            bgcolor: 'background.paper'
+        }}>
+            <Stack gap={3}>
+
+                <TextField label="Usuário" variant="outlined" fullWidth />
+                <TextField label="Senha" variant="outlined" fullWidth type="password" />
+            </Stack>
+
+            <Button variant="text" color="primary" fullWidth>Entrar</Button>
+        </Stack>
     )
 }
